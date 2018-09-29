4ac20d13f2a92dd26c68b21916161c60f03561eb
guga <guga.burigo29@outlook.com>
2018-09-29 14:56:56 -0300

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const routesApi = require('./src/Routes/routesApi')
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({
+    extended: true,
+}))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.use('/lista', routesApi)
+
+app.listen(8080, function () {
+    console.log(`Servidor rodando na porta http://localhost:8080`)
+})
