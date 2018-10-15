71370ae9b7380406e4406da1989ce81aa04023c6
guga <guga.burigo29@outlook.com>
2018-10-15 20:52:14 -0300

    Adiciona imagem nos produtos

--- src/Models/ListaModel.js
+++ src/Models/ListaModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const listaSchema = new mongoose.Schema({
     id: {type: Number},
     nome: {type: String},
+    imagem: {type: String},
     tipo: {type: String},
     preco: {type: Number},
     marca: {type: String}
